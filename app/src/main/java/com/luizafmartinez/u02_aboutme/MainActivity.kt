77e2374b2b3b102f99c411d040c77dcf35f6e748
alxmart <alxmartinez@gmail.com>
2024-03-05 16:52:17 -0300

    Hide the keyboard

--- app/src/main/java/com/luizafmartinez/u02_aboutme/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/u02_aboutme/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.luizafmartinez.u02_aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         btnDone.setOnClickListener {
             addNickname(it)
+
         }
     }
 
@@ -27,5 +30,9 @@ class MainActivity : AppCompatActivity() {
         editText.visibility = View.GONE
         view.visibility = View.GONE
         nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
